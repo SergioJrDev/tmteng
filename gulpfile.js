b66b0b01c7d56ded6a173663bf30cf80399467bc
Sergio Jr <sergioamjr91@gmail.com>
2017-09-21 22:35:07 -0300

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('stylus', function () {
+  return gulp.src('./css/style.styl')
+    .pipe(stylus({
+      compress: true
+    }))
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions', 'Firefox > 20', '> 5%'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('css/components/*.styl', ['stylus']);
+    gulp.watch('plugins/Components/*.styl', ['stylus']);
+    gulp.watch('css/shared/*.styl', ['stylus']);
+});
+
+
+gulp.task('default', ['stylus', 'watch']);
